7494a2402cd26e78f62f1f19cb4d8c0d9d30d9bb
Longwei Su <longwei.su@gmail.com>
2017-02-22 11:12:37 -0500

    add sample ios download

---
+++ DownloaderTest/DownloaderTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloaderTest
+//
+//  Created by longwei su on 2/21/17.
+//  Copyright © 2017 longwei su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DownloaderTest/DownloaderTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DownloaderTest
+//
+//  Created by longwei su on 2/21/17.
+//  Copyright © 2017 longwei su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <NSURLSessionDownloadDelegate, UIDocumentInteractionControllerDelegate>
+
+@end
+
